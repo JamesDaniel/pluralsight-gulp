a5b2af28236ec2a62c017f71d1f7002f13697759
JamesDaniel <james.d.mcgarr@students.ittralee.ie>
2017-02-20 01:31:06 +0000

    initial commit

---
+++ gulp.config.js
@@ -0,0 +1,18 @@
+module.exports = function () {
+    var client = './src/client/';
+    var config = {
+        temp: './.temp',
+
+        /*
+         * File paths
+         */
+        alljs: [
+            './src/**/*.js',
+            './*.js'
+        ],
+
+        less: client + 'styles/styles.less'
+    };
+
+    return config;
+};
